cad7db77e21f4ab6ff3d6145ceec68fb71f6a88d
GazdagB <gazdagbal@gmail.com>
2026-06-14 15:44:15 +0200

    ✨ add: first working api endpoint

--- app/api/expenses.py
+++ app/api/expenses.py
@@ -1,15 +1,23 @@
 from fastapi import APIRouter
-
+from app.repositories.expense_repository import get_all_expenses, insert_one_expense
+from pydantic import BaseModel
+from datetime import date
+from decimal import Decimal
 router = APIRouter(
     prefix="/expenses",
     tags=["expenses"],
 )
-
+#TODO: Move out these schemas from here
+class ExpenseCreate(BaseModel):
+    title: str
+    amount: Decimal
+    expense_date: date | None
+    category_id: int
 
 @router.get("/")
 def get_expenses():
-    return {"message": "GET /expenses"}
+    return get_all_expenses()
 
-@router.post("")
-def create_expense():
-    return {"message": "POST /expenses"}
+@router.post("/")
+def create_expense(expense: ExpenseCreate):
+    return insert_one_expense(expense)
--- app/repositories/expense_repository.py
+++ app/repositories/expense_repository.py
@@ -1,1 +1,24 @@
+from datetime import datetime
 
+from psycopg.rows import dict_row
+
+from app.database.db import get_connection
+
+def get_all_expenses():
+    with get_connection() as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            return cur.execute('SELECT * FROM expenses').fetchall()
+
+def insert_one_expense(expense):
+
+    title = expense.title
+    amount = expense.amount
+    expense_date = expense.expense_date
+    category_id = expense.category_id
+
+    if expense_date is None:
+        expense_date = datetime.now().date()
+
+    with get_connection() as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            return cur.execute("""INSERT INTO expenses (title, amount,expense_date,category_id) VALUES (%s, %s, %s, %s) RETURNING *""", (title,amount,expense_date, category_id)).fetchall()
