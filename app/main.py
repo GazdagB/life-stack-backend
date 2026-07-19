c7b2f19539051a948f69f4a198590a426bc76901
Gazdag Balázs <gazdagbal@gmail.com>
2026-07-19 22:18:17 +0200

    add: cors origin letting dev clients to test

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.expenses import router as expenses_router
 from app.api.test_db import router as test_db_router
 from app.api.todos import router as todos_router
 from app.api.auth import router as auth_router
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(expenses_router)
 app.include_router(test_db_router)
